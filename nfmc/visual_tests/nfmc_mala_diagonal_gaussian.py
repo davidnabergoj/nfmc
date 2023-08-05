9a0e74ff7995341620cb65c288e309aa9da58652
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-05 20:17:12 +0200

    Remove step size since it is auto tuned

--- nfmc/visual_tests/nfmc_mala_diagonal_gaussian.py
+++ nfmc/visual_tests/nfmc_mala_diagonal_gaussian.py
@@ -21,17 +21,19 @@ if __name__ == '__main__':
     target = DiagonalGaussian(mu, sigma)
     flow = Flow(RealNVP(n_dim, n_layers=1))
 
-    ret = mala(x0, flow, target, full_output=True, jump_period=jump_period, tau=1e-4)
+    ret = mala(x0, flow, target, full_output=True, jump_period=jump_period)
     print(f'{ret.shape = }')
     nf_resample_mask = (torch.arange(1, len(ret) + 1) % jump_period) == 0
     ret = ret[:, :4, :]  # first four chains only
 
     gt = target.sample(batch_shape=(10000,))
 
+    chain_id = 0
+
     plt.figure()
     plt.scatter(gt[..., 0], gt[..., 1], label='Ground truth', alpha=0.3)
-    plt.scatter(ret[~nf_resample_mask, :, 0], ret[~nf_resample_mask, :, 1], label='MALA samples')
-    plt.scatter(ret[nf_resample_mask, :, 0], ret[nf_resample_mask, :, 1], label='NF jumps', ec='w')
+    plt.scatter(ret[~nf_resample_mask, chain_id, 0], ret[~nf_resample_mask, chain_id, 1], label='MALA samples')
+    plt.scatter(ret[nf_resample_mask, chain_id, 0], ret[nf_resample_mask, chain_id, 1], label='NF jumps', ec='w')
     plt.legend()
     plt.tight_layout()
     # lim = 50
