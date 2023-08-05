91d56fee74ede0a93f682c9193a6a7c10a480923
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-05 18:52:04 +0200

    Add metropolization test

---
+++ nfmc/test/test_metropolization.py
@@ -0,0 +1,28 @@
+import torch
+
+from nfmc.util import metropolis_acceptance_log_ratio
+from potentials.synthetic.gaussian import StandardGaussian, DiagonalGaussian
+
+
+def test_log_ratio():
+    x0 = torch.tensor([[-100.0, -100.0]])
+    x1 = torch.tensor([[0.0, 0.0]])
+
+    target = StandardGaussian(event_shape=(2,))
+    proposal = DiagonalGaussian(mu=torch.zeros(2), sigma=torch.ones(2) * 100)
+
+    forward = metropolis_acceptance_log_ratio(
+        log_prob_curr=-target(x0),
+        log_prob_prime=-target(x1),
+        log_proposal_curr=-proposal(x0),
+        log_proposal_prime=-proposal(x1)
+    )
+
+    inverse = metropolis_acceptance_log_ratio(
+        log_prob_curr=-proposal(x0),
+        log_prob_prime=-proposal(x1),
+        log_proposal_curr=-target(x0),
+        log_proposal_prime=-target(x1)
+    )
+
+    assert forward > inverse
