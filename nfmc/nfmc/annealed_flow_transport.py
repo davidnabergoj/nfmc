09ec98f2334b576e5efa648d7d7bd1e54f5413fd
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-15 20:28:05 +0200

    Implement CRAFT

--- nfmc/nfmc/annealed_flow_transport.py
+++ nfmc/nfmc/annealed_flow_transport.py
@@ -2,21 +2,65 @@ import math
 from copy import deepcopy
 
 import torch
+import torch.optim as optim
 
 from nfmc.mcmc.hmc import hmc_trajectory
-from normalizing_flows import Flow
+from normalizing_flows import Flow, RealNVP
+from normalizing_flows.src.bijections import Bijection
 from potentials.base import Potential
 
 
-def aft(
-        prior_potential: Potential,
+def smc_flow_step(x, flow, prev_potential, next_potential, log_W, log_Z, sampling_threshold):
+    n_particles, n_dim = x.shape
+    x_tilde, log_det = flow.bijection.forward(x)
+
+    # next_lambda = k / n_steps
+    # next_potential = lambda v: (1 - next_lambda) * prior_potential(v) + next_lambda * target_potential(v)
+    #
+    # prev_lambda = (k - 1) / n_steps
+    # prev_potential = lambda v: (1 - prev_lambda) * prior_potential(v) + prev_lambda * target_potential(v)
+
+    log_G = next_potential(x_tilde) + log_det - prev_potential(x)
+    log_w = torch.logaddexp(log_W, log_G)
+    log_Z += torch.sum(log_w)
+
+    log_W = log_w - torch.logsumexp(log_w, dim=0)
+
+    # ess = 1 / sum(exp(2 * log_w))
+    # log_ess = -log(sum(exp(2*log_w)))
+    log_ess = - torch.logsumexp(2 * log_w, dim=0)
+
+    if log_ess - math.log(n_particles) <= math.log(sampling_threshold):
+        resampled_indices = torch.distributions.Categorical(logits=log_w).sample(size=(n_particles,)).long()
+        x = x_tilde[resampled_indices]
+        log_W = torch.full(size=(n_particles,), fill_value=math.log(1 / n_particles))
+    else:
+        x = x_tilde  # Not explicitly stated in pseudocode, but probably true
+
+    momentum = torch.randn_like(x)
+    inv_mass_diag = torch.ones(size=(1, n_dim))
+    trajectory_length = 10
+    step_size = n_dim ** (-1 / 4)
+    x, _ = hmc_trajectory(
+        x=x,
+        momentum=momentum,
+        inv_mass_diag=inv_mass_diag,
+        n_leapfrog_steps=trajectory_length,
+        step_size=step_size,
+        potential=next_potential
+    )
+
+    return x, log_Z, log_W
+
+
+def aft(prior_potential: Potential,
         target_potential: Potential,
         flow: Flow,
         n_particles: int = 100,
         n_steps: int = 20,
         sampling_threshold: float = 0.3,
         full_output: bool = False
-):
+        ):
     """
     Linear annealing schedule.
     Using HMC kernel.
@@ -31,54 +75,110 @@ def aft(
     """
     assert 1 / n_particles <= sampling_threshold < 1
     x = prior_potential.sample(batch_shape=(n_particles,))
-    n_dim = prior_potential.event_shape[0]  # TODO add support for general event shapes
     log_W = torch.full(size=(n_particles,), fill_value=math.log(1 / n_particles))
     log_Z = 0.0
-
-    # TODO check shapes
 
     xs = [deepcopy(x.detach())]
     for k in range(1, n_steps):
         flow.fit(x)
-        x_tilde, log_det = flow.bijection.forward(x)
-
-        next_lambda = k / n_steps
-        next_potential = lambda v: (1 - next_lambda) * prior_potential(v) + next_lambda * target_potential(v)
-
         prev_lambda = (k - 1) / n_steps
-        prev_potential = lambda v: (1 - prev_lambda) * prior_potential(v) + prev_lambda * target_potential(v)
-
-        log_G = next_potential(x_tilde) + log_det - prev_potential(x)
-        log_w = torch.logaddexp(log_W, log_G)
-        log_Z += torch.sum(log_w)
-
-        log_W = log_w - torch.logsumexp(log_w, dim=0)
-
-        # ess = 1 / sum(exp(2 * log_w))
-        # log_ess = -log(sum(exp(2*log_w)))
-        log_ess = - torch.logsumexp(2 * log_w, dim=0)
-
-        if log_ess - math.log(n_particles) <= math.log(sampling_threshold):
-            resampled_indices = torch.distributions.Categorical(logits=log_w).sample(size=(n_particles,)).long()
-            x = x_tilde[resampled_indices]
-            log_W = torch.full(size=(n_particles,), fill_value=math.log(1 / n_particles))
-        else:
-            x = x_tilde  # Not explicitly stated in pseudocode, but probably true
-
-        momentum = torch.randn_like(x)
-        inv_mass_diag = torch.ones(size=(1, n_dim))
-        trajectory_length = 10
-        step_size = n_dim ** (-1 / 4)
-        x, _ = hmc_trajectory(
+        next_lambda = k / n_steps
+        x, log_Z, log_W = smc_flow_step(
             x=x,
-            momentum=momentum,
-            inv_mass_diag=inv_mass_diag,
-            n_leapfrog_steps=trajectory_length,
-            step_size=step_size,
-            potential=next_potential
+            flow=flow,
+            prev_potential=lambda v: (1 - prev_lambda) * prior_potential(v) + prev_lambda * target_potential(v),
+            next_potential=lambda v: (1 - next_lambda) * prior_potential(v) + next_lambda * target_potential(v),
+            log_W=log_W,
+            log_Z=log_Z,
+            sampling_threshold=sampling_threshold
         )
         xs.append(deepcopy(x.detach()))
 
     if full_output:
         return torch.stack(xs)
+    return x
+
+
+def craft(prior_potential: Potential,
+          target_potential: Potential,
+          n_particles: int,
+          n_training_steps: int,
+          n_annealing_steps: int,
+          sampling_threshold: float = 0.3,
+          full_output: bool = False):
+    """
+    Idea:
+    * have a flow for each annealing step that connects the intermediate potentials
+    * the last flow approximates the target
+    * train these flows in J steps
+
+    :param n_particles:
+    :param n_annealing_steps: number of annealing steps/levels.
+    :return:
+    """
+    assert 1 / n_particles <= sampling_threshold < 1
+    n_dim = prior_potential.event_shape[0]
+    flows = [RealNVP(n_dim) for _ in range(n_annealing_steps)]
+    optimizers = [optim.AdamW(flow.parameters()) for flow in flows]
+
+    def loss(x_prev: torch.Tensor,
+             W_prev: torch.Tensor,  # Not the log!!!
+             bijection: Bijection,
+             u_prev: callable,
+             u_next: callable):
+        x_next, log_det = bijection.inverse(x_prev)
+        d = -u_prev(x_prev) + u_next(x_next) + log_det  # (n_particles,)
+        loss = torch.sum(W_prev * d)
+        return loss
+
+    # Train
+    for j in range(n_training_steps):
+        x = prior_potential.sample(batch_shape=(n_particles,))
+        log_W = torch.full(size=(n_particles,), fill_value=-math.log(n_particles))
+        log_Z = 0.0
+        for k in range(n_annealing_steps):
+            prev_lambda = (k - 1) / n_annealing_steps
+            prev_potential = lambda v: (1 - prev_lambda) * prior_potential(v) + prev_lambda * target_potential(v)
+
+            next_lambda = k / n_annealing_steps
+            next_potential = lambda v: (1 - next_lambda) * prior_potential(v) + next_lambda * target_potential(v)
+
+            optimizers[k].zero_grad()
+            loss_value = loss(x, log_W.exp(), flows[k], prev_potential, next_potential)
+            loss_value.backward()
+
+            x, log_Z, log_W = smc_flow_step(
+                x=x,
+                flow=flows[k],
+                prev_potential=prev_potential,
+                next_potential=next_potential,
+                log_W=log_W,
+                log_Z=log_Z,
+                sampling_threshold=sampling_threshold
+            )
+            optimizers[k].step()
+
+    # Sample
+    x = prior_potential.sample(batch_shape=(n_particles,))
+    log_W = torch.full(size=(n_particles,), fill_value=-math.log(n_particles))
+    log_Z = 0.0
+    for k in range(n_annealing_steps):
+        prev_lambda = (k - 1) / n_annealing_steps
+        prev_potential = lambda v: (1 - prev_lambda) * prior_potential(v) + prev_lambda * target_potential(v)
+
+        next_lambda = k / n_annealing_steps
+        next_potential = lambda v: (1 - next_lambda) * prior_potential(v) + next_lambda * target_potential(v)
+
+        x, log_Z, log_W = smc_flow_step(
+            x=x,
+            flow=flows[k],
+            prev_potential=prev_potential,
+            next_potential=next_potential,
+            log_W=log_W,
+            log_Z=log_Z,
+            sampling_threshold=sampling_threshold
+        )
+
+    if full_output:
+        return x, flows, log_Z
     return x
