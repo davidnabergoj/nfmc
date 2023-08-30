8c8d852377f6659956be8c2b109776246f69126c
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-30 12:02:12 +0200

    Compute bias2 per chain first, then average

--- nfmc/metrics.py
+++ nfmc/metrics.py
@@ -1,30 +1,66 @@
 import torch
 
 
-def empirical_mean(samples):
-    # samples.shape = (n_steps, n_chains, n_dim)
-    n_steps, n_chains, n_dim = samples.shape
-    tmp = torch.sum(samples, dim=1)
-    return torch.cumsum(tmp, dim=0) / (n_chains * torch.arange(n_steps))[:, None]
+def compute_empirical_moment(samples, k: int):
+    """
+    Compute empirical moment for each step of a chain.
+    :param samples:
+    :param k:
+    :return:
+    """
+    # samples.shape = (n_steps, n_dim)
+    # output.shape = (n_steps, n_dim)
+    n_steps, n_dim = samples.shape
+    return torch.cumsum(samples ** k, dim=0) / torch.arange(n_steps)[:, None]
 
 
-def empirical_2nd_moment(samples):
-    # samples.shape = (n_steps, n_chains, n_dim)
-    n_steps, n_chains, n_dim = samples.shape
-    tmp = torch.sum(samples ** 2, dim=1)
-    return torch.cumsum(tmp, dim=0) / (n_chains * torch.arange(n_steps))[:, None]
+def compute_empirical_mean(samples):
+    """
+    Compute empirical mean for each step of a chain.
+    :param samples:
+    :return:
+    """
+    # samples.shape = (n_steps, n_dim)
+    # output.shape = (n_steps, n_dim)
+    return compute_empirical_moment(samples, 1)
+
+
+def compute_empirical_2nd_moment(samples):
+    """
+    Compute empirical 2nd moment for each step of a chain.
+    :param samples:
+    :return:
+    """
+    # samples.shape = (n_steps, n_dim)
+    # output.shape = (n_steps, n_dim)
+    return compute_empirical_moment(samples, 2)
 
 
 def unnormalized_bias(empirical_moment, true_moment, order: int = 2):
+    """
+    Compute unnormalized bias for each step of a single chain.
+    :param empirical_moment:
+    :param true_moment:
+    :param order:
+    :return:
+    """
+    # samples.shape = (n_steps, n_dim)
+    # true_moment.shape = (n_dim,)
+    # output.shape = (n_steps, n_dim)
     # order = 2: squared bias
     # order = 1: squared bias, but numerator is the absolute value
-    # empirical_moment.shape = (n_steps, n_dim)
-    # true_moment.shape = (n_dim,)
     ret, _ = ((empirical_moment - true_moment[None]) ** order).abs().max(dim=-1)
     return ret
 
 
 def normalized_bias(empirical_moment, true_moment, true_variance, order: int = 2):
+    """
+    Compute normalized bias for each step of a single chain.
+    :param empirical_moment:
+    :param true_moment:
+    :param order:
+    :return:
+    """
     # order = 2: squared bias
     # order = 1: squared bias, but numerator is the absolute value
     # empirical_moment.shape = (n_steps, n_dim)
@@ -34,10 +70,16 @@ def normalized_bias(empirical_moment, true_moment, true_variance, order: int = 2
 
 
 def absolute_error_mean(samples: torch.Tensor, true_mean: torch.Tensor):
+    """
+    Compute normalized bias for each step of many chains.
+    """
     # samples.shape = (n_steps, n_chains, n_dim)
     # true_mean.shape = (n_dim,)
     # Computes max_{dim} (empirical_mean - true_mean).abs()
-    return unnormalized_bias(empirical_mean(samples), true_mean, order=1)
+    return torch.vmap(
+        lambda chain: unnormalized_bias(compute_empirical_mean(chain), true_mean, order=1),
+        in_dims=1
+    )(samples).mean(dim=1)
 
 
 def normalized_absolute_error_mean(samples: torch.Tensor, true_mean: torch.Tensor, true_variance: torch.Tensor):
@@ -47,27 +89,46 @@ def normalized_absolute_error_mean(samples: torch.Tensor, true_mean: torch.Tenso
     # Computes max_{dim} (empirical_mean - true_mean).abs() / true_variance
     # The idea is that some targets have dimensions with different magnitudes. Dividing by the true variance will bring
     # them to the same scale.
-    return normalized_bias(empirical_mean(samples), true_mean, true_variance, order=1)
+    return torch.vmap(
+        lambda chain: normalized_bias(compute_empirical_mean(chain), true_mean, true_variance, order=1),
+        in_dims=1
+    )(samples).mean(dim=1)
 
 
 def squared_bias_mean(samples: torch.Tensor, true_mean: torch.Tensor, true_variance: torch.Tensor):
     # samples.shape = (n_steps, n_chains, n_dim)
     # true_mean.shape = (n_dim,)
     # Computes max_{dim} (empirical_mean - true_mean) ** 2
-    return normalized_bias(empirical_mean(samples), true_mean, true_variance, order=2)
+    # return normalized_bias(compute_empirical_mean(samples), true_mean, true_variance, order=2)
+    return torch.vmap(
+        lambda chain: normalized_bias(compute_empirical_mean(chain), true_mean, true_variance, order=2),
+        in_dims=1
+    )(samples).mean(dim=1)
 
 
 def absolute_error_2nd_moment(samples: torch.Tensor, true_2nd_moment: torch.Tensor):
-    return unnormalized_bias(empirical_2nd_moment(samples), true_2nd_moment, order=1)
+    # return unnormalized_bias(compute_empirical_2nd_moment(samples), true_2nd_moment, order=1)
+    return torch.vmap(
+        lambda chain: unnormalized_bias(compute_empirical_2nd_moment(chain), true_2nd_moment, order=1),
+        in_dims=1
+    )(samples).mean(dim=1)
 
 
 def normalized_absolute_error_2nd_moment(samples: torch.Tensor, true_2nd_moment: torch.Tensor,
                                          true_variance: torch.Tensor):
-    return normalized_bias(empirical_2nd_moment(samples), true_2nd_moment, true_variance, order=1)
+    # return normalized_bias(compute_empirical_2nd_moment(samples), true_2nd_moment, true_variance, order=1)
+    return torch.vmap(
+        lambda chain: normalized_bias(compute_empirical_2nd_moment(chain), true_2nd_moment, true_variance, order=1),
+        in_dims=1
+    )(samples).mean(dim=1)
 
 
 def squared_bias_2nd_moment(samples: torch.Tensor, true_2nd_moment: torch.Tensor, true_variance: torch.Tensor):
-    return normalized_bias(empirical_2nd_moment(samples), true_2nd_moment, true_variance, order=2)
+    # return normalized_bias(compute_empirical_2nd_moment(samples), true_2nd_moment, true_variance, order=2)
+    return torch.vmap(
+        lambda chain: normalized_bias(compute_empirical_2nd_moment(chain), true_2nd_moment, true_variance, order=2),
+        in_dims=1
+    )(samples).mean(dim=1)
 
 
 def steps_to_low_squared_bias_2nd_moment(samples: torch.Tensor,
