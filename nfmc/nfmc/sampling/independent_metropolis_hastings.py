9a87010233fac27c2d215485f428edd58f14cef0
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-31 14:39:28 +0200

    Add aggressive IMH algorithm, let use pass kwargs to NF fit in IMH

--- nfmc/nfmc/sampling/independent_metropolis_hastings.py
+++ nfmc/nfmc/sampling/independent_metropolis_hastings.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from copy import deepcopy
 from tqdm import tqdm
 import torch
@@ -21,12 +22,13 @@ def imh(x0: torch.Tensor,
         n_iterations: int = 1000,
         full_output: bool = False,
         adaptation_dropoff: float = 0.9999,
-        train_dist: str = 'bounded_geom'):
+        train_dist: str = 'bounded_geom',
+        **kwargs):
     assert train_dist in ['bounded_geom_approx', 'bounded_geom', 'uniform']
     # Exponentially diminishing adaptation probability sequence
     # TODO make initial states be sampled from the flow
 
-    xs_all = []
+    xs = torch.zeros(size=(n_iterations, *x0.shape), dtype=x0.dtype)
 
     n_accepted = 0
     n_total = 0
@@ -46,12 +48,11 @@ def imh(x0: torch.Tensor,
             accepted_mask = torch.less(log_u, log_alpha)
             x[accepted_mask] = x_proposed[accepted_mask]
             x = x.detach()
-
-            xs_all.append(deepcopy(x))
+            xs[i] = deepcopy(x)
 
         n_accepted += int(torch.sum(accepted_mask.long()))
         n_total += n_chains
-        instantaneous_acceptance_rate = float(torch.mean(accepted_mask.float()))
+        # instantaneous_acceptance_rate = float(torch.mean(accepted_mask.float()))
 
         u_prime = torch.rand(size=())
         alpha_prime = adaptation_dropoff ** i
@@ -61,21 +62,82 @@ def imh(x0: torch.Tensor,
             # we can program the exact bounded geometric as well. Then it's parameter p can be adapted with dual
             # averaging.
             if train_dist == 'uniform':
-                k = int(torch.randint(low=0, high=len(xs_all), size=()))
+                k = int(torch.randint(low=0, high=(i + 1), size=()))
             elif train_dist == 'bounded_geom_approx':
-                k = int(torch.randint(low=max(0, len(xs_all) - 100), high=len(xs_all), size=()))
+                k = int(torch.randint(low=max(0, (i + 1) - 100), high=(i + 1), size=()))
             elif train_dist == 'bounded_geom':
-                k = sample_bounded_geom(p=0.025, max_val=len(xs_all) - 1)
+                k = sample_bounded_geom(p=0.025, max_val=(i + 1) - 1)
             else:
                 raise ValueError
-            x_train = xs_all[k]
-            flow.fit(x_train, n_epochs=1)
+            x_train = xs[k]
+            flow.fit(x_train, n_epochs=1, **kwargs)
 
-        pbar.set_postfix_str(f'accept-frac: {n_accepted / n_total:.3f} | adapt-prob: {alpha_prime:.3f}')
-
-    xs_all = torch.stack(xs_all, dim=0)
+        pbar.set_postfix_str(f'accept-frac: {n_accepted / n_total:.6f} | adapt-prob: {alpha_prime:.6f}')
 
     if full_output:
-        return xs_all
+        return xs
     else:
         return x
+
+
+def aggressive_imh(x0: torch.Tensor,
+                   flow: Flow,
+                   potential: callable,
+                   n_iterations: int = 1000,
+                   adaptation_dropoff: float = 0.9999,
+                   train_dist: str = 'uniform',
+                   **kwargs):
+    n_chains, *event_shape = x0.shape
+    xs = torch.zeros(size=(n_iterations, n_chains, *event_shape), dtype=x0.dtype)
+
+    n_accepted = 0
+    n_total = 0
+
+    train_indices = defaultdict(int)
+    x = deepcopy(x0)
+    for i in (pbar := tqdm(range(n_iterations))):
+        with torch.no_grad():
+            x_proposed = flow.sample(n_chains, no_grad=True)
+            log_alpha = metropolis_acceptance_log_ratio(
+                log_prob_curr=-potential(x),
+                log_prob_prime=-potential(x_proposed),
+                log_proposal_curr=flow.log_prob(x),
+                log_proposal_prime=flow.log_prob(x_proposed)
+            )
+            log_u = torch.rand(n_chains).log().to(log_alpha)
+            accepted_mask = torch.less(log_u, log_alpha)
+            x[accepted_mask] = x_proposed[accepted_mask]
+            x = x.detach()
+            xs[i] = deepcopy(x)
+
+        n_accepted += int(torch.sum(accepted_mask.long()))
+        n_total += n_chains
+        # instantaneous_acceptance_rate = float(torch.mean(accepted_mask.float()))
+
+        u_prime = torch.rand(size=())
+        alpha_prime = adaptation_dropoff ** i
+        if u_prime < alpha_prime:
+            # only use recent states to adapt
+            # this is an approximation of a bounded "geometric distribution" that picks the training data
+            # we can program the exact bounded geometric as well. Then it's parameter p can be adapted with dual
+            # averaging.
+            if train_dist == 'uniform':
+                k = int(torch.randint(low=0, high=(i + 1), size=()))
+            elif train_dist == 'bounded_geom_approx':
+                k = int(torch.randint(low=max(0, (i + 1) - 100), high=(i + 1), size=()))
+            elif train_dist == 'bounded_geom':
+                k = int(sample_bounded_geom(p=0.025, max_val=(i + 1) - 1))
+            else:
+                raise ValueError
+            train_indices[k] += 1
+
+            train_data_indices, train_data_weights = zip(*train_indices.items())
+            idx_train = torch.tensor(list(train_data_indices), dtype=torch.long)
+            w_train = torch.tensor(list(train_data_weights), dtype=torch.float)
+            w_train = w_train.unsqueeze(1).repeat(1, n_chains).view(-1)
+            x_train = xs[idx_train].view(-1, *event_shape)
+            flow.fit(x_train, n_epochs=30, w_train=w_train, **kwargs)
+
+        pbar.set_postfix_str(f'accept-frac: {n_accepted / n_total:.6f} | adapt-prob: {alpha_prime:.6f}')
+
+    return xs
