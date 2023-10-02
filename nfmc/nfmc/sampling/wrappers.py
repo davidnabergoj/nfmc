2f1be724caf2479ca9cf9cd3fdc151860145f39d
David Nabergoj <davidnabergoj4@gmail.com>
2023-10-02 17:16:44 +0200

    Add wrapper boilerplate

---
+++ nfmc/nfmc/sampling/wrappers.py
@@ -0,0 +1,21 @@
+from base import Potential
+
+
+def mala_wrapper(target: Potential, flow: str):
+    pass
+
+
+def ula_wrapper(target: Potential, flow: str):
+    pass
+
+
+def imh_wrapper(target: Potential, flow: str):
+    pass
+
+
+def neutra_hmc_wrapper(target: Potential, flow: str):
+    pass
+
+
+def tess_wrapper(target: Potential, flow: str):
+    pass
---
+++ nfmc/nfmc/transport/wrappers.py
@@ -0,0 +1,25 @@
+from base import Potential
+
+
+def aft_wrapper(target: Potential, flow: str):
+    pass
+
+
+def craft_wrapper(target: Potential, flow: str):
+    pass
+
+
+def dlmc_wrapper(target: Potential, flow: str):
+    pass
+
+
+def fab_wrapper(target: Potential, flow: str):
+    pass
+
+
+def ns_wrapper(target: Potential, flow: str):
+    pass
+
+
+def snf_wrapper(target: Potential, flow: str):
+    pass
