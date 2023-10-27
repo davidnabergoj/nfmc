8a2f96f03ee04aa6145050235b1683668ef4fd93
David Nabergoj <davidnabergoj4@gmail.com>
2023-10-27 13:58:16 +0200

    Fix SNF sampling

--- nfmc/mcmc/mh.py
+++ nfmc/mcmc/mh.py
@@ -10,4 +10,15 @@ def mh_step(x: torch.Tensor, proposal_std: torch.Tensor = None):
         assert proposal_std.shape == (1, n_dim)
     noise = torch.randn_like(x) * proposal_std
     x_prime = x + noise
+
+    # return proposed point and log of the metropolis acceptance ratio
+
     return x_prime
+
+
+def mh(
+        x: torch.Tensor,
+        proposal_std: torch.Tensor = None,
+        inv_mass_diag: torch.Tensor = None
+):
+    raise NotImplementedError
