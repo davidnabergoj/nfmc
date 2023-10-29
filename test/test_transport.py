bfcb597a4ce0a2f6865a14237979966a7e9934ba
David Nabergoj <dn9134@student.uni-lj.si>
2023-10-29 18:20:35 +0100

    Fix tests

--- test/test_transport.py
+++ test/test_transport.py
@@ -1,29 +1,16 @@
-from nfmc.transport import ns, snf, aft, dlmc, craft
+from nfmc.transport import ns, snf, aft, craft
 
 import torch
 import pytest
 
 from nfmc.util import get_supported_normalizing_flows
-from synthetic.gaussian.unit import StandardGaussian
+from potentials.synthetic.gaussian.unit import StandardGaussian
 
 all_event_shapes = [(2,), (5,), (2, 3, 7)]
 all_n_particles = [5, 50]
 all_n_iterations = [5, 1, 50]
 all_flows = get_supported_normalizing_flows()
 all_jump_periods = [10, 50]
-
-
-@pytest.mark.parametrize('event_shape', all_event_shapes)
-@pytest.mark.parametrize('n_particles', all_n_particles)
-@pytest.mark.parametrize('flow', all_flows)
-def test_dlmc(event_shape, n_particles, flow):
-    torch.manual_seed(0)
-    prior = StandardGaussian(event_shape)
-    target = StandardGaussian(event_shape)
-    particle_history = dlmc(prior, target, flow, n_particles)
-    assert particle_history.shape[1:] == (n_particles, *event_shape)
-    assert torch.all(~torch.isnan(particle_history))
-    assert torch.all(torch.isfinite(particle_history))
 
 
 @pytest.mark.parametrize('event_shape', all_event_shapes)
