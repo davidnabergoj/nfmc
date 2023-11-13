3d565f374076dbd3d78d3f69a4c04af2f8c2d4c7
David Nabergoj <davidnabergoj4@gmail.com>
2023-11-13 12:15:00 -0800

    Add option to specify inverse mass diagonal in LMC and to return kernel parameters, rename tau to step_size

--- nfmc/mcmc/langevin_algorithm.py
+++ nfmc/mcmc/langevin_algorithm.py
@@ -22,27 +22,30 @@ def proposal_potential(x_prime: torch.Tensor,
 def base(x0: torch.Tensor,
          potential: callable,
          n_iterations: int = 1000,
-         tau: float = None,
+         step_size: float = None,
          full_output: bool = True,
          adjustment: bool = False,
+         inv_mass_diag: torch.Tensor = None,
          tune_diagonal_preconditioning: bool = True,
-         target_acceptance_rate: float = 0.574):
+         target_acceptance_rate: float = 0.574,
+         return_kernel_parameters: bool = False):
     assert torch.all(torch.isfinite(x0))
 
     # TODO tune tau in MALA
     n_chains, *event_shape = x0.shape
     n_dim = int(torch.prod(torch.as_tensor(event_shape)))
-    if tau is None:
-        tau = n_dim ** (-1 / 3)
+    if step_size is None:
+        step_size = n_dim ** (-1 / 3)
     xs = []
 
-    # Compute standard deviation of chain states unless using a single chain
-    if x0.shape[0] > 1 and tune_diagonal_preconditioning:
-        sqrt_a = 1 / torch.std(x0, dim=0)  # root of the preconditioning matrix diagonal
-    else:
-        sqrt_a = torch.ones(*event_shape)
+    if inv_mass_diag is None:
+        # Compute standard deviation of chain states unless using a single chain
+        if x0.shape[0] > 1 and tune_diagonal_preconditioning:
+            inv_mass_diag = torch.std(x0, dim=0)  # root of the mass matrix diagonal
+        else:
+            inv_mass_diag = torch.ones(*event_shape)
 
-    dual_avg = DualAveraging(math.log(tau))
+    dual_avg = DualAveraging(math.log(step_size))
 
     x = deepcopy(x0)
     for i in range(n_iterations):
@@ -57,7 +60,9 @@ def base(x0: torch.Tensor,
         x.grad = None  # Clear gradients
 
         # Compute new state
-        x_prime = x - tau * sqrt_a[None].square() * grad_u_x + math.sqrt(2 * tau) * sqrt_a[None] * noise
+        grad_term = -step_size / inv_mass_diag[None].square() * grad_u_x
+        noise_term = math.sqrt(2 * step_size) / inv_mass_diag[None] * noise
+        x_prime = x + grad_term + noise_term
 
         if adjustment:
             # Compute potential and gradient at proposed state
@@ -72,28 +77,33 @@ def base(x0: torch.Tensor,
             log_ratio = metropolis_acceptance_log_ratio(
                 log_prob_curr=-u_x,
                 log_prob_prime=-u_x_prime,
-                log_proposal_curr=-proposal_potential(x, x_prime, grad_u_x_prime, sqrt_a ** 2, tau),
-                log_proposal_prime=-proposal_potential(x_prime, x, grad_u_x, sqrt_a ** 2, tau)
+                log_proposal_curr=-proposal_potential(x, x_prime, grad_u_x_prime, 1 / inv_mass_diag ** 2, step_size),
+                log_proposal_prime=-proposal_potential(x_prime, x, grad_u_x, 1 / inv_mass_diag ** 2, step_size)
             )
             adjustment_mask = torch.as_tensor(torch.log(torch.rand(n_chains)) < log_ratio)
             acceptance_rate = float(torch.mean(adjustment_mask.float()))
             dual_avg.step(target_acceptance_rate - acceptance_rate)
-            tau = math.exp(dual_avg.value)
+            step_size = math.exp(dual_avg.value)
         else:
             # No adjustment (ULA)
             adjustment_mask = torch.ones(n_chains, dtype=torch.bool)
         x[adjustment_mask] = x_prime[adjustment_mask]
 
+        # Update the inerse mass diagonal
         if x0.shape[0] > 1 and tune_diagonal_preconditioning:
-            sqrt_a = 1 / torch.std(x, dim=0)  # root of the preconditioning matrix diagonal
-        else:
-            sqrt_a = torch.ones(*event_shape)
+            inv_mass_diag = torch.std(x, dim=0)  # root of the preconditioning matrix diagonal
 
         if full_output:
             xs.append(deepcopy(x))
 
     if full_output:
-        return torch.stack(xs)
+        x = torch.stack(xs)
+
+    if return_kernel_parameters:
+        return x, {
+            "inv_mass_diag": inv_mass_diag,
+            "step_size": step_size
+        }
     else:
         return x
 
