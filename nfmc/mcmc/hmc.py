5e564993ac01b45b9a472d1421067f9dab405399
David Nabergoj <davidnabergoj4@gmail.com>
2024-03-22 00:39:30 +0100

    Stabler HMC implementation

--- nfmc/mcmc/hmc.py
+++ nfmc/mcmc/hmc.py
@@ -50,87 +50,81 @@ def hmc_trajectory(x: torch.Tensor,
 
 def hmc(x0: torch.Tensor,
         potential: callable,
-        n_iterations: int = 1000,
-        n_leapfrog_steps: int = 15,
-        step_size: float = None,
-        full_output: bool = True,
+        n_iterations: int = 100,
+        n_leapfrog_steps: int = 50,
+        step_size: float = 0.01,
         target_acceptance_rate: float = 0.651,
-        show_progress: bool = False,
+        da_kwargs: dict = None,
         inv_mass_diag: torch.Tensor = None,
-        return_kernel_parameters: bool = False,
-        adjustment: bool = True):
+        tune_inv_mass_diag: bool = True,
+        tune_step_size: bool = True,
+        show_progress: bool = True,
+        adjustment: bool = True,
+        full_output: bool = False):
+    if da_kwargs is None:
+        da_kwargs = dict()
+
     n_chains, *event_shape = x0.shape
-
-    xs = torch.zeros(size=(n_iterations, *x0.shape), dtype=x0.dtype)
-    xs[0] = x0
-    x = torch.clone(x0)
-    n_dim = int(torch.prod(torch.as_tensor(event_shape)))
-    if step_size is None:
-        step_size = n_dim ** (-1 / 4)
+    xs = torch.zeros(size=(n_iterations, n_chains, *event_shape))
+    da = DualAveraging(initial_step_size=step_size, **da_kwargs)
+    x = torch.clone(x0).detach()
     if inv_mass_diag is None:
-        inv_mass_diag = torch.ones(size=event_shape)  # has shape: event_shape
+        inv_mass_diag = torch.ones(size=event_shape)
 
-    if show_progress:
-        iterator = tqdm(range(n_iterations), desc='HMC')
-    else:
-        iterator = range(n_iterations)
+    n_divergences = 0
+    accepted_total = 0
+    for i in (pbar := tqdm(range(n_iterations), desc='HMC', disable=not show_progress)):
+        p = torch.randn_like(x) / inv_mass_diag.sqrt()[None]  # Momentum
 
-    total_accepted = 0
-    total_seen = 0
+        try:
+            x_prime, p_prime = hmc_trajectory(
+                x,
+                p,
+                inv_mass_diag=inv_mass_diag,
+                step_size=step_size,
+                n_leapfrog_steps=n_leapfrog_steps,
+                potential=potential
+            )
 
-    for i in iterator:
-        initial_momentum = torch.randn_like(x) / inv_mass_diag.sqrt()[None]
-        x_prime, momentum_prime = hmc_trajectory(
-            x,
-            initial_momentum,
-            inv_mass_diag,
-            step_size,
-            n_leapfrog_steps,
-            potential
-        )
-        if adjustment:
             with torch.no_grad():
-                log_alpha = metropolis_acceptance_log_ratio(
-                    log_prob_curr=-potential(x) - 0.5 * sum_except_batch(
-                        initial_momentum ** 2 * inv_mass_diag,
-                        event_shape=event_shape
-                    ),
-                    log_prob_prime=-potential(x_prime) - 0.5 * sum_except_batch(
-                        momentum_prime ** 2 * inv_mass_diag,
-                        event_shape=event_shape
-                    ),
-                    log_proposal_curr=0.0,
-                    log_proposal_prime=0.0
-                )  # batch_shape
-                log_u = torch.rand_like(log_alpha).log()  # n_chains
-                accepted_mask = torch.less(log_u, log_alpha)  # n_chains
+                if adjustment:
+                    hamiltonian_start = potential(x) + 0.5 * sum_except_batch(p ** 2 * inv_mass_diag[None], event_shape)
+                    hamiltonian_end = potential(x_prime) + 0.5 * sum_except_batch(p_prime ** 2 * inv_mass_diag[None],
+                                                                                  event_shape)
+                    log_prob_accept = -hamiltonian_end - (-hamiltonian_start)
+                    log_u = torch.rand_like(log_prob_accept).log()  # n_chains
+                    accepted_mask = (log_u < log_prob_accept)  # n_chains
+                else:
+                    accepted_mask = torch.ones(size=(n_chains,), dtype=torch.bool)
+
                 x[accepted_mask] = x_prime[accepted_mask]
-                x = x.detach()
+        except ValueError:
+            accepted_mask = torch.zeros(size=(n_chains,), dtype=torch.bool)
+            n_divergences += 1
 
-                # inv_mass_diag = torch.std(x0, dim=0)
-                acceptance_rate = float(torch.mean(accepted_mask.float()))
+        with torch.no_grad():
+            accepted_total += int(torch.sum(accepted_mask))
+            x = x.detach()
+            xs[i] = x
+            acc_rate = torch.mean(accepted_mask.float())
 
-                total_accepted += int(torch.sum(accepted_mask))
-        else:
-            x = x_prime
-            total_accepted += n_chains
-        total_seen += n_chains
-
-        if show_progress:
-            iterator.set_postfix_str(f'accept-frac: {total_accepted / total_seen:.4f}')
-
-        if full_output:
-            xs[i] = deepcopy(x.detach())
-
+            if tune_inv_mass_diag:
+                inv_mass_diag = torch.var(x, dim=0)  # Mass matrix adaptation
+            if tune_step_size and adjustment:  # Step size tuning is only possible with adjustment right now
+                error = target_acceptance_rate - acc_rate
+                da.step(error)
+                step_size = da.value  # Step size adaptation
+            pbar.set_postfix_str(
+                f'acc: {accepted_total / (n_chains * (i + 1)):.3f} [{acc_rate:.3f}], '
+                f'log step: {math.log(step_size):.2f}, '
+                f'imd_max_norm: {torch.max(torch.abs(inv_mass_diag)):.2f}, '
+                f'da_error: {da.error_sum:.2f}, '
+                # f'da_log_step: {da.log_step:.2f}, '
+                f'n_divergences: {n_divergences}'
+            )
     if full_output:
-        ret_x = xs.detach()
-    else:
-        ret_x = x.detach()
-
-    if return_kernel_parameters:
-        return ret_x, {
-            "inv_mass_diag": inv_mass_diag,
-            "step_size": step_size
+        return xs, {
+            'inv_mass_diag': inv_mass_diag,
+            'step_size': step_size
         }
-    else:
-        return ret_x
+    return xs
