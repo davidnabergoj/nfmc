5075a17e228d92105c756a398fe1fab1f57a06e5
David Nabergoj <davidnabergoj4@gmail.com>
2024-03-22 00:40:27 +0100

    Add classic MCMC to sample method

--- nfmc/sample.py
+++ nfmc/sample.py
@@ -1,4 +1,4 @@
-from typing import Union, List, Tuple
+from typing import Union, List, Tuple, Optional
 
 import torch
 
@@ -10,11 +10,12 @@ from nfmc.sampling_implementations.jump.langevin_monte_carlo import unadjusted_l
 from nfmc.sampling_implementations.neutra import neutra_hmc_base
 from nfmc.util import create_flow_object
 from normalizing_flows import Flow
+from nfmc.mcmc import hmc, mh, nuts, mala
 
 
 def sample(target: callable,
-           flow: Union[str, Flow],
            event_shape,
+           flow: Optional[Union[str, Flow]] = None,
            strategy: str = "imh",
            n_chains: int = 100,
            n_iterations: int = 100,
@@ -24,11 +25,15 @@ def sample(target: callable,
            **kwargs):
     # strategy_choices = ["imh", "jump_mala", "jump_ula", "jump_hmc", "jump_uhmc", "tess", "neutra_hmc"]
 
+    if flow is None and strategy not in ['hmc', 'uhmc', 'ula', 'mala', 'mh']:
+        raise ValueError
+
     # Create the flow object if necessary
-    if isinstance(flow, str):
-        flow_object = create_flow_object(flow_name=flow, event_shape=event_shape, edge_list=edge_list).to(device)
-    else:
-        flow_object = flow.to(device)
+    if flow is not None:
+        if isinstance(flow, str):
+            flow_object = create_flow_object(flow_name=flow, event_shape=event_shape, edge_list=edge_list).to(device)
+        else:
+            flow_object = flow.to(device)
 
     # Create a set of initial chain states if necessary
     if x0 is None:
@@ -90,6 +95,45 @@ def sample(target: callable,
             n_chains,
             n_vi_iterations=n_iterations,
             n_hmc_iterations=n_iterations,
+            **kwargs
+        )
+    elif strategy == "hmc":
+        return hmc(
+            x0,
+            target,
+            n_iterations=n_iterations,
+            adjustment=True,
+            **kwargs
+        )
+    elif strategy == "uhmc":
+        return hmc(
+            x0,
+            target,
+            n_iterations=n_iterations,
+            adjustment=False,
+            **kwargs
+        )
+    elif strategy == "mala":
+        return mala(
+            x0,
+            target,
+            n_iterations=n_iterations,
+            adjustment=True,
+            **kwargs
+        )
+    elif strategy == "ula":
+        return mala(
+            x0,
+            target,
+            n_iterations=n_iterations,
+            adjustment=False,
+            **kwargs
+        )
+    elif strategy == "mh":
+        return mh(
+            x0,
+            target,
+            n_iterations=n_iterations,
             **kwargs
         )
     else:
