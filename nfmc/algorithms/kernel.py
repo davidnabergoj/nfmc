7c10a5ff451402b835ea96cf5cca53b875233ffc
David Nabergoj <davidnabergoj4@gmail.com>
2025-05-24 17:29:10 +0200

    Add composition and mixing kernels

--- nfmc/algorithms/kernel.py
+++ nfmc/algorithms/kernel.py
@@ -1,5 +1,5 @@
-from typing import Tuple, Union
-
+from typing import Tuple, Union, List
+import math
 import torch
 
 
@@ -67,3 +67,142 @@ class MarkovKernel:
 
     def __repr__(self):
         raise NotImplementedError
+
+
+class CompositionKernel(MarkovKernel):
+    """
+    Composition of two or more Markov kernels.
+
+    This means applying Markov kernels one after another in a Markov chain.
+    Note: All kernels must use the same target log probability density callable object (checked by object identity).
+    """
+
+    def __init__(self,
+                 kernels: List[MarkovKernel],
+                 mode: str = 'full'):
+        """
+        CompositionKernel constructor.
+
+        :param List[MarkovKernel] kernels: list of Markov kernel objects.
+        :param str mode: one of ['full', 'cyclic']. If 'full', applies all kernels in a single step. If 'cyclic', 
+         applies only the next kernel in a single step.
+        """
+        if len(kernels) < 1:
+            raise ValueError("At least one kernel must be provided.")
+        for k in kernels[1:]:
+            if not (k.event_shape is kernels[0].event_shape):
+                raise ValueError(
+                    "All kernels must have the same event shape."
+                )
+            if not (k.neg_log_prob_target is kernels[0].neg_log_prob_target):
+                raise ValueError(
+                    "All kernels must use the same negative log probability density callable."
+                )
+        if mode not in ['full', 'cyclic']:
+            raise ValueError("mode must be one of ['full', 'cyclic']")
+
+        super().__init__(
+            event_shape=kernels[0].event_shape,
+            neg_log_prob_target=kernels[0].neg_log_prob_target
+        )
+        self.kernels = kernels
+        self.kernel_index = 0
+        self.mode = mode
+
+    def step(self,
+             x: torch.Tensor,
+             update: bool = False,
+             **kwargs):
+        """
+        Performs a transition with the next kernel if in cyclic mode.
+        Performs a transition with all kernels if in full mode.
+
+        :param torch.Tensor x: current state tensor with shape `(*batch_shape, *event_shape)`.
+        :param bool update: if True, update this kernel's parameters.
+        :return: new state tensor with shape `(*batch_shape, *event_shape)`.
+        """
+
+        if self.mode == 'cyclic':
+            out = self.kernels[self.kernel_index].step(
+                x=x,
+                update=update,
+                **kwargs
+            )
+            self.kernel_index = (self.kernel_index + 1) % len(self.kernels)
+            return out
+        else:
+            y = x
+            for k in self.kernels:
+                y = k.step(y, update=update, **kwargs)
+            return y
+
+
+class MixingKernel(MarkovKernel):
+    """
+    Mixing of two or more Markov kernels.
+
+    This means applying one Markov kernel at each step of a Markov chain. Each kernel has an associated selection 
+    probability. These probabilities determine which kernel is chosen for the transition.
+    Note: all kernels must use the same target log probability density callable object (checked by object identity).
+    """
+
+    def __init__(self,
+                 kernels: List[MarkovKernel],
+                 selection_probabilities: List[float] = None):
+        """
+        MixingKernel constructor.
+
+        :param List[MarkovKernel] kernels: list of Markov kernel objects.
+        :param List[float] selection_probabilities: list of selection probabilities, one probability for each kernel.
+         These probabilities must sum to 1. If None, set uniform selection probabilities.
+        """
+        if len(kernels) < 1:
+            raise ValueError("At least one kernel must be provided.")
+        for k in kernels[1:]:
+            if not (k.event_shape is kernels[0].event_shape):
+                raise ValueError(
+                    "All kernels must have the same event shape."
+                )
+            if not (k.neg_log_prob_target is kernels[0].neg_log_prob_target):
+                raise ValueError(
+                    "All kernels must use the same negative log probability density callable."
+                )
+        if selection_probabilities is None:
+            selection_probabilities = [
+                1 / len(kernels) for _ in kernels]
+        else:
+            if len(selection_probabilities) != len(kernels):
+                raise ValueError(
+                    "The number of kernels and selection probabilities must be the same.")
+            if not math.isclose(sum(selection_probabilities), 1.0):
+                raise ValueError("Selection probabilities must sum to 1")
+
+        super().__init__(
+            event_shape=kernels[0].event_shape,
+            neg_log_prob_target=kernels[0].neg_log_prob_target
+        )
+        self.kernels = kernels
+        self.dist = torch.distributions.Categorical(
+            probs=torch.tensor(
+                selection_probabilities,
+                dtype=torch.float
+            )
+        )
+
+    def step(self,
+             x: torch.Tensor,
+             update: bool = False,
+             **kwargs):
+        """
+        Performs a transition with a randomly chosen kernel.
+
+        :param torch.Tensor x: current state tensor with shape `(*batch_shape, *event_shape)`.
+        :param bool update: if True, update this kernel's parameters.
+        :return: new state tensor with shape `(*batch_shape, *event_shape)`.
+        """
+        idx = self.dist.sample()
+        return self.kernels[idx].step(
+            x=x,
+            update=update,
+            **kwargs
+        )
--- nfmc/algorithms/mh/jump.py
+++
@@ -1,6 +0,0 @@
-from nfmc.algorithms.mh.base import MHKernel
-
-
-class JumpMHKernel(MHKernel):
-    def __init__(self, event_shape, neg_log_prob_target):
-        super().__init__(event_shape, neg_log_prob_target)
