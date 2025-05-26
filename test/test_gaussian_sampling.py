2adbd7f51c3958ef5741f499fc7d29b63c4fd587
David Nabergoj <davidnabergoj4@gmail.com>
2025-05-26 17:27:19 +0200

    Change preconditioning parameters in test

--- test/test_gaussian_sampling.py
+++ test/test_gaussian_sampling.py
@@ -14,13 +14,23 @@ from nfmc.algorithms.preconditioning.samplers.neutra import (
     NeuTraMALA,
     NeuTraRWMH,
 )
+from nfmc.algorithms.preconditioning.samplers.diagonal import (
+    DiagonalHMC,
+    DiagonalMALA,
+    DiagonalRWMH,
+)
+from nfmc.algorithms.preconditioning.samplers.dense import (
+    DenseHMC,
+    DenseMALA,
+    DenseRWMH,
+)
 
 from test.util import DiagonalGaussian
 
 
 @pytest.mark.parametrize("kernel_class", [RWMHKernel, MALAKernel, HMCKernel])
 @pytest.mark.local_only
-def test_mcmc(kernel_class):
+def test_local_mh(kernel_class):
     torch.manual_seed(0)
 
     event_shape = (4,)
@@ -38,18 +48,74 @@ def test_mcmc(kernel_class):
     )
 
     assert torch.allclose(
-        sampling_draws.first_moment.as_tensor(), target.first_moment, rtol=0.2
+        sampling_draws.first_moment.as_tensor(),
+        target.first_moment,
+        rtol=0.2
     )
     assert torch.allclose(
-        sampling_draws.second_moment.as_tensor(), target.second_moment, rtol=0.2
+        sampling_draws.second_moment.as_tensor(),
+        target.second_moment,
+        rtol=0.2
     )
 
-@pytest.mark.skip
+
 @pytest.mark.local_only
 @pytest.mark.parametrize(
-    "sampler_class", [NeuTraRWMH, NeuTraMALA, NeuTraHMC]
+    "sampler_class", [
+        DiagonalRWMH,
+        DiagonalMALA,
+        DiagonalHMC,
+        DenseRWMH,
+        DenseMALA,
+        DenseHMC,
+    ]
 )
-def test_preconditioned_mcmc_warmup_and_sample(sampler_class):
+def test_linear_prec_mh_warmup_and_sample(sampler_class):
+    torch.manual_seed(0)
+
+    event_shape = (4,)
+    n_chains = 10
+    target = DiagonalGaussian(event_shape)
+
+    sampler = sampler_class(
+        event_shape=event_shape,
+        neg_log_prob_target=target.neg_log_prob
+    )
+
+    z0 = torch.rand(size=(n_chains, *event_shape)) * 2 - 1
+    warmup_draws, latent_warmup_draws = sampler.warmup(
+        z0=z0,
+        n_steps=200,
+        preconditioner_update_interval=50,
+        return_latent_samples=True,
+    )
+    sampling_draws, latent_sampling_draws = sampler.sample(
+        z0=latent_warmup_draws.last_sample,
+        n_steps=200,
+        return_latent_samples=True
+    )
+
+    assert torch.allclose(
+        sampling_draws.first_moment.as_tensor(),
+        target.first_moment,
+        rtol=0.2
+    )
+    assert torch.allclose(
+        sampling_draws.second_moment.as_tensor(),
+        target.second_moment,
+        rtol=0.2
+    )
+
+
+@pytest.mark.local_only
+@pytest.mark.parametrize(
+    "sampler_class", [
+        NeuTraRWMH,
+        NeuTraMALA,
+        NeuTraHMC,
+    ]
+)
+def test_neutra_mh_warmup_and_sample(sampler_class):
     torch.manual_seed(0)
 
     event_shape = (4,)
@@ -69,8 +135,11 @@ def test_preconditioned_mcmc_warmup_and_sample(sampler_class):
         preconditioner_update_interval=300,
         return_latent_samples=True,
     )
-    sampling_draws = sampler.sample(
-        z0=latent_warmup_draws.last_sample, n_steps=2000)
+    sampling_draws, latent_sampling_draws = sampler.sample(
+        z0=latent_warmup_draws.last_sample,
+        n_steps=2000,
+        return_latent_samples=True
+    )
 
     flow_samples = flow.sample((10000,)).detach()
     flow_first_moment = flow_samples.mean(0)
@@ -80,13 +149,17 @@ def test_preconditioned_mcmc_warmup_and_sample(sampler_class):
     assert torch.allclose(target.second_moment, flow_second_moment, rtol=0.2)
 
     assert torch.allclose(
-        sampling_draws.first_moment.as_tensor(), target.first_moment, rtol=0.2
+        sampling_draws.first_moment.as_tensor(),
+        target.first_moment,
+        rtol=0.2
     )
     assert torch.allclose(
-        sampling_draws.second_moment.as_tensor(), target.second_moment, rtol=0.2
+        sampling_draws.second_moment.as_tensor(),
+        target.second_moment,
+        rtol=0.2
     )
 
 
 @pytest.mark.local_only
-def test_jump_mcmc():
+def test_jump_mh():
     torch.manual_seed(0)
