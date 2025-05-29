868f5c6a4ddcede1cdc251fb460a62b6daf35e8f
David Nabergoj <davidnabergoj4@gmail.com>
2025-05-29 18:02:44 +0200

    Merge tests

--- test/test_preconditioned_mcmc.py
+++ test/test_preconditioned_mcmc.py
@@ -56,57 +56,42 @@ def test_warmup(event_shape,
 
 
 @pytest.mark.local_only
-@pytest.mark.parametrize('event_shape', [(2,)])
-@pytest.mark.parametrize('sampler_class', [NeuTraRWMH, NeuTraMALA, NeuTraHMC])
+@pytest.mark.parametrize('event_shape', [(2,), (2, 3, 5)])
+@pytest.mark.parametrize('sampler_class', [
+    DiagonalRWMH,
+    DiagonalMALA,
+    DiagonalHMC,
+    DenseRWMH,
+    DenseMALA,
+    DenseHMC,
+    NeuTraRWMH,
+    NeuTraMALA,
+    NeuTraHMC,
+])
 @pytest.mark.parametrize('n_chains', [1, 4])
 @pytest.mark.parametrize('n_steps', [1, 4])
-def test_sample_linear_2(event_shape,
-                         sampler_class,
-                         n_chains,
-                         n_steps):
+def test_sample_shape(event_shape,
+                      sampler_class,
+                      n_chains,
+                      n_steps):
     torch.manual_seed(0)
     original_neg_log_prob_target = StandardGaussian(event_shape).neg_log_prob
 
-    flow = create_flow_object('realnvp', event_shape)
-    sampler = sampler_class(
-        flow=flow,
-        neg_log_prob_target=original_neg_log_prob_target,
-    )
+    if sampler_class in [NeuTraHMC, NeuTraRWMH, NeuTraMALA]:
+        flow = create_flow_object('realnvp', event_shape)
+        sampler = sampler_class(
+            flow=flow,
+            neg_log_prob_target=original_neg_log_prob_target,
+        )
+    else:
+        sampler = sampler_class(
+            event_shape,
+            original_neg_log_prob_target
+        )
+
     assert sampler.kernel._preconditioner is not None
     assert sampler.kernel._preconditioner.inverse_transform is not None
     assert sampler.kernel.neg_log_prob_target is not original_neg_log_prob_target
-
-    z_initial = torch.randn(size=(n_chains, *event_shape))
-    samples = sampler.sample(
-        z_initial,
-        n_steps=n_steps,
-        show_progress=False
-    )
-
-    assert isinstance(samples, Samples)
-    assert torch.isfinite(samples.as_tensor()).all()
-    assert samples.as_tensor().shape == (n_steps, n_chains, *event_shape)
-    assert samples.as_tensor().dtype == z_initial.dtype
-
-
-@pytest.mark.local_only
-@pytest.mark.parametrize('event_shape', [(1,), (2,), (10,), (2, 3, 5)])
-@pytest.mark.parametrize('kernel_class', [RWMHKernel, HMCKernel, MALAKernel])
-@pytest.mark.parametrize('n_chains', [1, 2, 4])
-@pytest.mark.parametrize('n_steps', [1, 2, 4])
-@pytest.mark.parametrize('preconditioner_class', [
-    DiagonalLinearPreconditioner,
-    DenseLinearPreconditioner,
-])
-def test_sample_linear(event_shape, kernel_class, n_chains, n_steps, preconditioner_class):
-    torch.manual_seed(0)
-
-    kernel = kernel_class(
-        event_shape=event_shape,
-        neg_log_prob_target=StandardGaussian(event_shape).neg_log_prob,
-        preconditioner=preconditioner_class(event_shape)
-    )
-    sampler = PreconditionedMCMCSampler(kernel)
 
     z_initial = torch.randn(size=(n_chains, *event_shape))
     samples = sampler.sample(
