08a79b18c9c8cb9f02ebe3dfd9111fd719331e62
David Nabergoj <davidnabergoj4@gmail.com>
2025-05-29 18:04:38 +0200

    Remove redundant test

--- test/test_preconditioned_mcmc.py
+++ test/test_preconditioned_mcmc.py
@@ -105,36 +105,6 @@ def test_sample_shape(event_shape,
     assert samples.as_tensor().shape == (n_steps, n_chains, *event_shape)
     assert samples.as_tensor().dtype == z_initial.dtype
 
-
-@pytest.mark.parametrize('event_shape', [(2,), (10,), (2, 3, 5)])
-@pytest.mark.parametrize('kernel_class', [RWMHKernel, HMCKernel, MALAKernel, IMHKernel])
-@pytest.mark.parametrize('n_chains', [1, 2, 4])
-@pytest.mark.parametrize('n_steps', [1, 2, 4])
-def test_sample_nf(event_shape, kernel_class, n_chains, n_steps):
-    torch.manual_seed(0)
-
-    flow = create_flow_object('realnvp', event_shape)
-
-    kernel = kernel_class(
-        event_shape=event_shape,
-        neg_log_prob_target=StandardGaussian(event_shape).neg_log_prob,
-        preconditioner=NormalizingFlowPreconditioner(flow)
-    )
-    sampler = PreconditionedMCMCSampler(kernel)
-
-    z_initial = torch.randn(size=(n_chains, *event_shape))
-    samples = sampler.sample(
-        z_initial,
-        n_steps=n_steps,
-        show_progress=False
-    )
-
-    assert isinstance(samples, Samples)
-    assert torch.isfinite(samples.as_tensor()).all()
-    assert samples.as_tensor().shape == (n_steps, n_chains, *event_shape)
-    assert samples.as_tensor().dtype == z_initial.dtype
-
-
 @pytest.mark.local_only
 @pytest.mark.parametrize(
     "sampler_class", [
