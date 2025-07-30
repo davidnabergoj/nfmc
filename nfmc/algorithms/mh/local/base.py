Dual averaging fixes

nfmc/algorithms/mh/local/base.py
@@ -34,8 +34,6 @@ class LocalMHKernel(MHKernel):
         """
         super().__init__(event_shape, neg_log_prob_target, **kwargs)
 
-        self._warmup_flag = False
-
         # Store initial step size
         step_size = torch.as_tensor(step_size)
         self._initial_step_size = step_size
@@ -58,13 +56,16 @@ class LocalMHKernel(MHKernel):
         Use the weighted average of observed step sizes after warmup.
         """
         if len(self._dual_averaging.error_history) > 0:
-            self.step_size = self._dual_averaging.weighted_value()
+            self.step_size = self._dual_averaging.weighted_value().mean()
 
     def pbar_repr(self, elapsed_time_seconds: float):
-        da_eps = torch.mean(torch.as_tensor(self._dual_averaging.error_sum))
+        eps_mean = torch.mean(torch.as_tensor(self._dual_averaging.error_sum))
+        eps_max = torch.max(torch.as_tensor(self._dual_averaging.error_sum))
+        eps_min = torch.min(torch.as_tensor(self._dual_averaging.error_sum))
         data = [
             self.name,
-            f'log step: {math.log(self.step_size):.3f} [eps: {da_eps:.2f}]',
+            f'log step: {math.log(self.step_size):.3f}',
+            f'DA[e: {eps_mean:.2f} ^{eps_max:.2f} v{eps_min:.2f}]',
             f'{self.calls_per_second(elapsed_time_seconds):.3f} c/s',
             f'{self.grads_per_second(elapsed_time_seconds):.3f} g/s',
             f'{self.acceptance_rate:.3f} acc',

nfmc/algorithms/mh/local/dual_averaging.py
@@ -51,6 +51,8 @@ class DualAveraging:
         """
         Return step size history tensor with shape `(n_steps, n_chains)`.
         """
+        if len(self._step_size_history) == 0:
+            return self._step_size_history
         return torch.stack(self._step_size_history)
 
     @property
@@ -58,6 +60,8 @@ class DualAveraging:
         """
         Return error history tensor with shape `(n_steps, n_chains)`.
         """
+        if len(self._error_history) == 0:
+            return self._error_history
         return torch.stack(self._error_history)
 
     def step(self, acceptance_mask: torch.Tensor):
@@ -104,7 +108,7 @@ class DualAveraging:
         """
         Return step size tensor with shape `(n_chains,)`.
         """
-        return torch.exp(self.log_step_averaged)
+        return torch.exp(torch.as_tensor(self.log_step_averaged))
 
     def weighted_value(self, sigma: float = 1.0):
         """
@@ -123,8 +127,8 @@ class DualAveraging:
             )
 
         # Prepare variables
-        _errs = self.error_history.astype(torch.float32)
-        _log_steps = torch.log(self.step_size_history.astype(torch.float32))
+        _errs = self.error_history.float()
+        _log_steps = torch.log(self.step_size_history.float())
 
         _ws = torch.exp(-(_errs ** 2) / (2 * sigma ** 2))
         _ws = torch.exp(-(_errs ** 2) / (2 * sigma ** 2))
@@ -132,7 +136,7 @@ class DualAveraging:
 
         # Compute weighted step
         _w_log_step = torch.sum(_ws * _log_steps, dim=0) / _w_sum
-        _w_log_step[_w_sum == 0] = _log_steps[-1]  # Use last log step size if weights are zero for a chain
+        _w_log_step[_w_sum == 0] = _log_steps[-1][_w_sum == 0]  # Use last log step size if weights are zero for a chain
 
         _w_step = torch.exp(_w_log_step)
         return _w_step

nfmc/algorithms/mh/local/mala.py
@@ -8,13 +8,16 @@ from nfmc.util import compute_divergence_mask, grad_f, metropolis_acceptance_log
 
 def propose_state(x: torch.Tensor,
                   event_shape: Union[Tuple[int, ...], torch.Size],
-                  step_size: Union[torch.Tensor, float],
+                  step_size: torch.Tensor,
                   neg_log_prob_target: callable):
     """
     
     :param torch.Tensor x: event tensor with shape `(n_chains, *event_shape)`.
-    :param torch.Tensor step_size: step size scalar or tensor with shape `(n_chains,)`.
+    :param torch.Tensor step_size: step size tensor with shape `(n_chains,)` or `()`.
     """
+    if len(step_size.shape) == 1:
+        step_size = step_size.view(step_size.shape[0], *[1] * (len(x.shape) - 1))
+
     x = x.detach()
     noise = torch.randn_like(x).to(x)
 
@@ -31,16 +34,19 @@ def proposal_neg_log_prob(x_prime: torch.Tensor,
                           event_shape: Union[Tuple[int, ...], torch.Size],
                           x: torch.Tensor,
                           grad_u_x: torch.Tensor,
-                          tau: Union[torch.Tensor, float]):
+                          tau: torch.Tensor):
     """
     Compute the negative log probability density of the MALA proposal q(x_prime | x).
     
     :param torch.Tensor x: event tensor with shape `(n_chains, *event_shape)`.
     :param torch.Tensor x_prime: proposed event tensor with shape `(n_chains, *event_shape)`.
-    :param torch.Tensor tau: step size scalar or tensor with shape `(n_chains,)`.
+    :param torch.Tensor tau: step size tensor with shape `(n_chains,)` or `()`.
     """
+    if len(tau.shape) == 1:
+        tau = tau.view(tau.shape[0], *[1] * (len(x.shape) - 1))
+    
     term = x_prime - (x - tau * grad_u_x)
-    return sum_except_batch(term ** 2, event_shape) / (4 * tau)
+    return sum_except_batch(term ** 2 / (4 * tau), event_shape)
 
 
 class MALAKernel(LocalMHKernel):
@@ -82,7 +88,7 @@ class MALAKernel(LocalMHKernel):
         :param bool update: if True, update kernel parameters.
         :return: new state tensor with shape `(*batch_shape, *event_shape)`.
         """
-        if self._warmup_flag:
+        if self.warmup_active:
             step_size = self._dual_averaging.value
         else:
             step_size = self.step_size

nfmc/algorithms/mh/local/rwmh.py
@@ -5,8 +5,15 @@ from nfmc.algorithms.mh.local.base import LocalMHKernel
 from nfmc.util import compute_divergence_mask, metropolis_acceptance_log_ratio
 
 
-def propose_state(x: torch.Tensor,
-                  step_size: Union[torch.Tensor, float]):
+def propose_state(x: torch.Tensor, step_size: torch.Tensor):
+    """
+    
+    :param torch.Tensor x: event tensor with shape `(n_chains, *event_shape)`.
+    :param torch.Tensor step_size: step size tensor with shape `(n_chains,)` or `()`.
+    """
+    if len(step_size.shape) == 1:
+        step_size = step_size.view(step_size.shape[0], *[1] * (len(x.shape) - 1))
+    
     x_prime = x + step_size * torch.randn_like(x)
     return x_prime
 
@@ -56,7 +63,7 @@ class RWMHKernel(LocalMHKernel):
         :param bool update: if True, update kernel parameters.
         :return: new state tensor with shape `(*batch_shape, *event_shape)`.
         """
-        if self._warmup_flag:
+        if self.warmup_active:
             step_size = self._dual_averaging.value
         else:
             step_size = self.step_size

nfmc/algorithms/preconditioning/samplers/base.py
@@ -95,7 +95,7 @@ class PreconditionedMCMCSampler(MCMCSampler):
         :param kwargs: keyword arguments for `preconditioner.fit`.
         :return: Samples object with MCMC draws.
         """
-        self._warmup_flag = True
+        self.kernel.start_warmup()
 
         target_samples = Samples(
             event_shape=self.kernel.event_shape,
@@ -137,7 +137,10 @@ class PreconditionedMCMCSampler(MCMCSampler):
 
             # Sample indices with replacement
             indices = torch.multinomial(
-                probabilities, num_samples=len(states), replacement=True)
+                probabilities,
+                num_samples=len(states),
+                replacement=True
+            )
             return states[indices]
 
         for cycle_index in range(n_cycles):
@@ -180,10 +183,6 @@ class PreconditionedMCMCSampler(MCMCSampler):
                 # Step. Update if in first half of cycle.
                 do_update = step_index < cycle_length // 2
                 if step_index == (cycle_length // 2):
-                    # Resample states again
-                    # x = current_warmup_kernel._preconditioner.inverse_transform(z.clone())[0]
-                    # x = resample(x.clone())
-                    # z = current_warmup_kernel._preconditioner.forward_transform(x.clone())[0]
                     pass
 
                 z, x = current_warmup_kernel.step_with_preconditioner_inverse(
@@ -206,8 +205,9 @@ class PreconditionedMCMCSampler(MCMCSampler):
                 if time_limit_seconds is not None and elapsed_time > time_limit_seconds:
                     break
 
-        self._warmup_flag = False
-        
+        self.kernel.end_warmup()
+        self.kernel.finalize_parameters()
+
         if return_latent_samples:
             return target_samples, latent_samples
         return target_samples